59712f1a51c2a874cb15ecc88c0cf001410e2b02
Valeska Halamicek <vjhalamicek@gmail.com>
2018-10-01 19:17:07 -0500

    fixed it

--- main.py
+++ main.py
@@ -28,73 +28,63 @@ def validate():
     email_error = ''
     
     if is_blank(username):
-        user_error = "Please enter a valid username"
-        return render_template('home_form.html', user_error=user_error, email=email)
+        user_error = 'Please enter a valid username'
+
+    elif len(username) < 3:
+        user_error = 'Username must contain 3 or more characters'
+
+    elif len(username) > 20:
+        user_error = 'Username must be less than 20 characters'
+
+    elif ' ' in username:
+        user_error = "Username cannot contain spaces"
+
     else:
-        if len(username) < 3:
-            user_error = 'Username must contain 3 characters or more'
-            return render_template('home_form.html', user_error=user_error, username=username, email=email)
-
-        if len(username) > 20:
-            user_error = 'Username must contain less than 20 characters'
-            return render_template('home_form.html', user_error=user_error, username=username, email=email)
-
-        else:
-            if ' ' in username:
-                user_error = "Username cannot contain spaces"
-                return render_template('home_form.html', user_error=user_error, username=username, email=email)
-
-    if not is_blank(email): 
-        if len(email) < 3:
-            email_error = 'E-mail must be longer than 3 characters'
-            return render_template('home_form.html', username=username, email_error=email_error, email=email)
-        
-        if len(email) > 20:
-            email_error = "E-mail must be less than 20 character"
-            return render_template('home_form.html', username=username, email_error=email_error, email=email)
-
-        else:
-            if " " in email:
-                email_error = 'E-mail cannot contain spaces'
-                return render_template('home_form.html', username=username, email_error=email_error, email=email)
-            else:
-                if ('@' and '.') not in email:
-                    email_error = 'Not a valid email, please re-enter.'
-                    return render_template('home_form.html', username=username, email_error=email_error, email=email)
+        user_error = ''
 
     if is_blank(password):
-        password_error = "Please enter a valid password"
-        return render_template('home_form.html', username=username, password_error=password_error, email=email)
+        password_error = 'Please enter a valid password'
 
-    if not is_blank(password) and is_blank(ver_password):
-        ver_blank_error = "Please verify your password"
-        return render_template('home_form.html', username=username, user_error=user_error, 
-        password_error=password_error, ver_blank_error=ver_blank_error, email=email)
-    
+    elif len(password) < 3:
+        password_error = 'Password must contain 3 characters or more'
+
+    elif len(password) > 20:
+        password_error = 'Password must be less than 20 characters'
+
+    elif ' ' in password:
+        password_error = 'Password cannot contain spaces'
+
+    elif is_blank(ver_password):
+        ver_blank_error = 'Please verify your password'
+        
+    elif password != ver_password:
+        password_error = 'Passwords do not match please re-enter'
+
     else:
-        if len(password) < 3:
-            password_error = "Passwords must contain 3 characters or more"
-            return render_template('home_form.html', username=username, password_error=password_error, email=email)
-
-        if len(password) > 20:
-            password_error = "Passwords must contain less than 20 characters"
-            return render_template('home_form.html', username=username, password_error=password_error, email=email)
-
-        else:
-            if ' ' in password:
-                password_error = "Passwords cannot contain spaces"
-                return render_template('home_form.html', username=username, password_error=password_error, email=email)
-            
-            else:
-                if password != ver_password:
-                    password_error = "Passwords do not match, please re-enter."
-                    return render_template('home_form.html', username=username, password_error=password_error, email=email)
-
+        password_error = ''
+        ver_blank_error = ''
     
-        if not user_error and not password_error and not email_error:
-            return render_template('welcome_page.html', username=username)
+    if not is_blank(email):
+        if len(email) < 3:
+            email_error = 'E-mail must contain 3 characters or more'
 
+        elif len(email) > 20:
+            email_error = 'E-mail must be less than 20 characters'
 
+        elif ' ' in email:
+            email_error = 'E-mail cannot contain spaces'
+
+        elif '@' not in email or '.' not in email:
+            email_error = 'Not a valid e-mail'
+
+        else: email_error = ''
+
+    if not user_error and not password_error and not ver_blank_error and not email_error:
+        return render_template('welcome_page.html', username=username)
+
+    else:
+        return render_template('home_form.html', user_error=user_error, password_error=password_error, email_error=email_error, 
+        ver_blank_error = ver_blank_error, username=username, email=email)
 
 
 
